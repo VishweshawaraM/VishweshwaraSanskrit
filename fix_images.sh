664a078de4bd8cf33efec95a84ef3fe2bca1fc5e
Vishweshwara M <visanskrit.solopreneur@gmail.com>
2026-07-11 16:43:38 +0530

    Change image extensions to uppercase
    Updated image file extensions from lowercase to uppercase in HomeView.tsx.

--- fix_images.sh
+++ fix_images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i 's|\/90BC0280-743E-496C-AD5A-10DB96ED68C9\.png|/hero-1.png|g' src/views/HomeView.tsx
-sed -i 's|\/BE39A249-BCEF-417F-B31A-70D71B6A709D\.jpeg|/hero-2.jpeg|g' src/views/HomeView.tsx
-sed -i 's|\/C2A70D39-41A8-4BC8-B4C6-E11D27F21CA5\.png|/hero-3.png|g' src/views/HomeView.tsx
+sed -i 's|\/90BC0280-743E-496C-AD5A-10DB96ED68C9\.png|/hero-1.PNG|g' src/views/HomeView.tsx
+sed -i 's|\/BE39A249-BCEF-417F-B31A-70D71B6A709D\.jpeg|/hero-2.PNG|g' src/views/HomeView.tsx
+sed -i 's|\/C2A70D39-41A8-4BC8-B4C6-E11D27F21CA5\.png|/hero-3.PNG|g' src/views/HomeView.tsx
